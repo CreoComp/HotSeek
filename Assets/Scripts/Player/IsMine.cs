aa1930783241fd27a77c0c5a99f8b881538fab40
CreoComp <q2e4563@gmail.com>
2024-07-04 12:39:13 +0300

    фикс камеры

--- Assets/Scripts/Player/IsMine.cs
+++ Assets/Scripts/Player/IsMine.cs
@@ -6,6 +6,7 @@ public class IsMine : MonoBehaviour
     [SerializeField] private PlayerMovement movement;
     [SerializeField] private PhotonView view;
     [SerializeField] private GameObject cam;
+    [SerializeField] private CameraDemo cameraDemo;
 
     private void Awake()
     {
@@ -13,6 +14,7 @@ public class IsMine : MonoBehaviour
         {
             movement.enabled = false;
             cam.SetActive(false);
+            cameraDemo.enabled = false;
         }
     }
 }
