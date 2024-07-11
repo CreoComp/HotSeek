7012b72bccafe5b281e43c1be816fc82bd0b684b
CreoComp <q2e4563@gmail.com>
2024-07-12 01:35:43 +0300

    фикс спавнера бустеров

--- Assets/Scripts/Player/Boosters/BoosterSpawner.cs
+++ Assets/Scripts/Player/Boosters/BoosterSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BoosterSpawner : MonoBehaviour
 {
     private GameObject[] boosters;
+
+    private GameObject activeBooster;
     void Start()
     {
         boosters = Resources.LoadAll<GameObject>("Boosters");
@@ -18,7 +20,8 @@ public class BoosterSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(15, 30));
-            PhotonNetwork.Instantiate(boosters[Random.Range(0, boosters.Length)].name, transform.position, Quaternion.identity);
+            if(activeBooster == null)
+            activeBooster = PhotonNetwork.Instantiate(boosters[Random.Range(0, boosters.Length)].name, transform.position, Quaternion.identity);
         }
     }
 }
